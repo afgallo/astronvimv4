d897d640b213829745fef604e517a556474df989
Andre Gallo <9999415+afgallo@users.noreply.github.com>
2024-05-15 06:28:07 +1000

    customised setup

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Mason plugins
 
 ---@type LazySpec
@@ -12,6 +10,8 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "omnisharp",
+        "tailwindcss",
         -- add more arguments for adding more language servers
       })
     end,
@@ -25,6 +25,8 @@ return {
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "prettier",
         "stylua",
+        "csharpier",
+        "cfn-lint",
         -- add more arguments for adding more null-ls sources
       })
     end,
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- Customize Treesitter
 
 ---@type LazySpec
@@ -10,6 +8,19 @@ return {
     opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
       "lua",
       "vim",
+      "bash",
+      "c_sharp",
+      "css",
+      "dockerfile",
+      "html",
+      "javascript",
+      "json",
+      "python",
+      "ruby",
+      "sql",
+      "terraform",
+      "typescript",
+      "yaml",
       -- add more arguments for adding more treesitter parsers
     })
   end,
